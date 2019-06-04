e027eb51fd7cacb4b5229b1fedbae1a40fb563fa
soufian17 <soufian.tichattibin@student.hu.nl>
2019-06-04 20:22:58 +0200

    role based auth

--- src/app/guards/student.guard.ts
+++ src/app/guards/student.guard.ts
@@ -1,23 +1,21 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AuthorizationService } from '../service/authorization.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class FirebaseLoggedInGuard implements CanActivate {
+export class IsStudentGuard implements CanActivate {
 
   constructor(
     private authService: AuthorizationService
   ) {
   }
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-
-    if (!this.authService.isLoggedIn) {
-      return false;
+    if (this.authService.currentUser && this.authService.currentUser.role === 'student') {
+      return true;
     }
-    return true;
-
+    return false;
   }
 
 }
